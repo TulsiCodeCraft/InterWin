952cb7093022e36799c3bb09a74bf25e6d8fe40d
TulsiCodeCraft <152911625+TulsiCodeCraft@users.noreply.github.com>
2024-10-09 18:51:12 +0530

    changes made

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { register,login } from "../controllers/auth.controller";
+import { register,login } from "../controllers/auth.controller.js";
 const router = express.Router()
 
 router.post("/register", register );
